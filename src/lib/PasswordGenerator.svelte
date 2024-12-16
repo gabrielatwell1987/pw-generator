5879628134a21a11a77b3f402ad813bea2538fed
gabrielatwell1987 <gabrielatwell@proton.me>
2024-12-16 12:26:55 -0800

    edited the look

--- src/lib/PasswordGenerator.svelte
+++ src/lib/PasswordGenerator.svelte
@@ -80,105 +80,115 @@
 		border: 3px solid #666;
 		border-radius: 10px;
 		padding: 2rem;
-	}
 
-	.number {
-		display: flex;
-		align-items: center;
-		justify-content: space-evenly;
-	}
+		.number {
+			display: flex;
+			align-items: center;
+			justify-content: space-evenly;
+		}
 
-	.center {
-		text-align: center;
-	}
+		.center {
+			text-align: center;
+		}
 
-	label {
-		display: block;
-		margin-left: 1.1rem;
-		margin-bottom: 0.5rem;
-		letter-spacing: 2px;
-		font-family: Verdana, Geneva, Tahoma, sans-serif;
-		font-size: 1rem;
-		display: flex;
-		align-items: center;
-	}
+		label {
+			display: block;
+			margin-left: 1.1rem;
+			margin-bottom: 0.5rem;
+			letter-spacing: 2px;
+			font-family: Verdana, Geneva, Tahoma, sans-serif;
+			font-size: 1rem;
+			display: flex;
+			align-items: center;
 
-	label:first-child {
-		margin-bottom: -0.75rem;
-		margin-left: 0;
-	}
+			&:first-child {
+				margin-bottom: -0.25rem;
+				margin-left: 0;
 
-	b {
-		font-size: clamp(1.5rem, 1.2vw, 1.3rem);
-	}
+				input {
+					border-radius: 10px;
+				}
+			}
 
-	button {
-		display: block;
-		background-color: #028b04;
-		color: white;
-		border: none;
-		border-radius: 5px;
-		padding: 10px 24px;
-		font-size: clamp(1rem, 1.25vw, 1.15rem);
-		font-weight: 800;
-		letter-spacing: 1px;
-		cursor: pointer;
-		text-align: center;
-		margin-bottom: 0.25rem;
-	}
+			&:nth-child(5) {
+				margin-bottom: 1rem;
+			}
+		}
 
-	button:hover {
-		background: white;
-		color: #028b04;
-	}
+		b {
+			font-size: clamp(1rem, 1.2vw, 1.3rem);
+			font-weight: bold;
+		}
 
-	button:first-child {
-		margin-top: 1rem;
-	}
+		button {
+			display: block;
+			background-color: #028b04;
+			color: white;
+			border: none;
+			border-radius: 5px;
+			padding: 10px 24px;
+			font-size: clamp(1rem, 1.25vw, 1.15rem);
+			font-weight: 800;
+			letter-spacing: 1px;
+			cursor: pointer;
+			text-align: center;
+			margin-bottom: 0.5rem;
 
-	.save {
-		font-size: clamp(1rem, 1.25vw, 1.15rem);
+			&:hover {
+				background: white;
+				color: #028b04;
+			}
+		}
 
-		background-color: #0120d3;
-		color: white;
-	}
+		.save {
+			font-size: clamp(1rem, 1.25vw, 1.15rem);
 
-	.save:hover {
-		background: white;
-		color: #1020d3;
-	}
+			background-color: #0120d3;
+			color: white;
+		}
 
-	input[type='number'] {
-		width: 50px;
-		padding: 5px;
-		font-size: 1rem;
-		font-weight: 600;
-		letter-spacing: 2px;
-		text-align: center;
-	}
+		.save:hover {
+			background: white;
+			color: #1020d3;
+		}
 
-	input[type='checkbox'] {
-		appearance: none;
-		-webkit-appearance: none;
-		margin-right: 0.5rem;
-		width: 1rem;
-		height: 1rem;
-		background-color: #ccc;
-		cursor: pointer;
-		border-radius: 5px;
-		border: 2px solid #fff;
-	}
+		input[type='number'] {
+			width: 50px;
+			padding: 5px;
+			font-size: 1rem;
+			font-weight: 600;
+			letter-spacing: 2px;
+			text-align: center;
+		}
 
-	input[type='checkbox']:checked {
-		background-color: #444;
-		border: 2px solid #fff;
-	}
+		input[type='checkbox'] {
+			appearance: none;
+			-webkit-appearance: none;
+			margin-right: 0.5rem;
+			width: 1rem;
+			height: 1rem;
+			background-color: #ccc;
+			cursor: pointer;
+			border-radius: 5px;
+			border: 2px solid #fff;
+		}
 
-	h4 {
-		letter-spacing: 3px;
-	}
+		input[type='checkbox']:checked {
+			background-color: #028b04;
+			border: 2px solid #fff;
+		}
 
-	p {
-		font-family: Verdana, Geneva, Tahoma, sans-serif;
+		h4 {
+			color: #666;
+			letter-spacing: 3px;
+			font-size: clamp(0.9rem, 1vw, 1.1rem);
+			font-weight: 100;
+			text-align: center;
+			margin-bottom: -1rem;
+		}
+
+		p {
+			font-family: Verdana, Geneva, Tahoma, sans-serif;
+		}
 	}
 </style>
